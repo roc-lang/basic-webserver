318e3300b83abee06f6f1ecb8938f84cff354ee6
Luke Boswell <lukewilliamboswell@gmail.com>
2024-05-31 13:09:42 +1000

    refactor host files out into crates

--- crates/roc_host_bin/build.rs
+++ crates/roc_host_bin/build.rs
@@ -1,9 +1,9 @@
 fn main() {
+    println!("cargo:rustc-link-search=crates/roc_host_bin/");
+
     #[cfg(not(windows))]
     println!("cargo:rustc-link-lib=dylib=app");
 
     #[cfg(windows)]
     println!("cargo:rustc-link-lib=dylib=libapp");
-
-    println!("cargo:rustc-link-search=.");
 }
